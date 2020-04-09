7c9891c18eafcd17873e2f64355b3ba6015f396e
Sailesh Kasaju <leosailesh@gmail.com>
2020-04-09 18:48:10 +0545

    minor fix

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import { navigate } from 'gatsby-link';
 import PreviewCompatibleImage from '../../components/PreviewCompatibleImage';
 import Layout from '../../components/Layout';
